fde818f6d56a7532f134dca24575899441cfb97c
Ivan Ramiro Jimenez <irjimenez@smdigital.com.co>
2019-05-03 10:07:37 -0500

    App ajax query bd working on log

--- task-search.php
+++ task-search.php
@@ -7,9 +7,10 @@ $search = $_POST['search'];
 if(!empty($search)){
     //query buscando en la BD el valor digitado por el usuario
     $query = "select * from task where name like '$search%'";
+    //SELECT * FROM `task` where name like 'write%'
     $result = mysqli_query($conn,$query);
     //si hay algún error en el query acaba el proceso
-    if(!result){
+    if(!$result){
         die('Query error'.mysqli_error($conn));
     }
     //recorrer el resultado como un array
@@ -17,9 +18,9 @@ if(!empty($search)){
     while ($row=mysqli_fetch_array($result)) {
         $json [] = array(
         //convertir el array a una variable 
-            'name'=> row['name'],
-            'description'=> row['description'],
-            'id'=> row['id']
+            'name'=> $row['name'],
+            'description'=> $row['description'],
+            'id'=> $row['id'],
         );
     }
     //convertir la variable con los datos json
