4f4beaeeb14d3a4df1370a5bfbeab33b090f7570
Ivan Ramiro Jimenez <irjimenez@smdigital.com.co>
2019-08-13 17:16:20 -0500

    pdo connection 2

--- task-list.php
+++ task-list.php
@@ -1,21 +1,26 @@
 <?php
 include_once('database.php');
+$json = array();
 
 $objCon = new conexion();
 $con=$objCon->conectar();
 
 $query="select * from task";
-$stm = $objcon->prepare($query);
-$result=$objcon->execute($query);
+$con->prepare($query);
+$result=$con->execute($query);
            
 
-//$result=$pdo->prepare($query);
-if($result){
-    header("location: error.php");
-    die('Query failed').mysqli_error($con);
+if(!$result){
+     // die('Query failed').mysqli_error($con);
+      $json [] = array(
+        'name' => 'fail',
+        'description' => 'fail',
+        'id' => '1',
+    );
+    $jsonstring = Json_encode($json);
+    echo $jsonstring;
 }
 
-$json = array();
 while ($row=$result->fetch(PDO::FETCH_ASSOC)) {
     $json [] = array(
         'name' => $row['name'],
