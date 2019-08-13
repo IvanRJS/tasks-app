fa045dc98180d632904e0bbf0f987149d6462267
Ivan Ramiro Jimenez <irjimenez@smdigital.com.co>
2019-08-13 17:56:19 -0500

    trying to , add task

--- task-add.php
+++ task-add.php
@@ -1,14 +1,22 @@
 <?php
-include('database.php');
+include('conexion.php');
+
+$objCon=new conexion();
+$con=$objCon->conectar();
 
 echo $_POST['name'];
     if(isset($_POST['name'])){
     $name = $_POST['name'];
     $description= $_POST['description'];
-    
-    $query = "insert into task (name,description) values ('$name','$description')";
 
-    $result=mysqli_query($conn,$query);
+
+    $result = $con->prepare('INSERT INTO task (name,description)
+    values (:name,:description)');
+$result->bindParam(':name', $name, PDO::PARAM_STR);
+$result->bindParam(':description', $description, PDO::PARAM_STR);
+$result->execute();
+
+
     if(!$result){
         die('Query failed');
     }else{
