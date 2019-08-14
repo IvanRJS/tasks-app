46effc6d2f3cd507c7902b6f80da9d384109190c
Ivan Ramiro Jimenez <irjimenez@smdigital.com.co>
2019-08-14 09:36:40 -0500

    :search

--- task-search.php
+++ task-search.php
@@ -10,10 +10,10 @@ $objCon= $con->conectar();
 
 if(!empty($search)){
     //query searching the given value 
-    $query = "select * from task where name like :search";
+    $query = "SELECT * FROM task WHERE name LIKE :search";
     //SELECT * FROM `task` where name like 'write%'
     $result = $objCon->prepare($query);
-    $result->bindParam('search',"%".$search."%",PDO::PARAM_STR);
+    $result->bindParam(':search',$search."%",PDO::PARAM_STR);
     $result->execute();
     //if there is some error ends the proccess
     if(!$result){
